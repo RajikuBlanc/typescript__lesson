42c4701d7f6aa4608350729c64f1c2c2d8f72b92
本馬遼太郎 <rajiku.blanc@gmail.com>
2021-11-25 09:06:46 +0900

    first-commit

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface Props {
   text: string;
 }
 
+interface UserData {
+  id: number;
+  name: string;
+}
+
 const TestComponent: React.FC<Props> = (props) => {
+  const [count, setCount] = useState<number | null>(0);
+  const [user, setUser] = useState<UserData>({ id: 1, name: 'dummy' });
+  const [inputData, setInputData] = useState('');
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputData(e.target.value);
+  };
   return (
     <div>
       <p>{props.text}</p>
+      <p>Count: {count}</p>
+      <p>Id: {user.id}</p>
+      <p>Name: {user.name}</p>
+      <input type='text' value={inputData} onChange={handleInputChange} />
+      <p>{inputData}</p>
     </div>
   );
 };
